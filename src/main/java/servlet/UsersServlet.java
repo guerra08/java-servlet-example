f8960f3c405401f90ad4ad56abdac7abadcaf2ed
Bruno Guerra <bruno.guerra@edu.pucrs.br>
2020-10-25 20:37:16 -0300

    Fix: url pattern

--- src/main/java/servlet/UsersServlet.java
+++ src/main/java/servlet/UsersServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.stream.Collectors;
 
-@WebServlet("/users")
+@WebServlet("/users/*")
 public class UsersServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
